44b04c8fd8befee2c0c66125395999cc47b0bb00
ollieOsh <sosinusi@gmail.com>
2017-07-11 10:35:34 -0500

    .

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> Showroom = new HashSet<string>();
+            Showroom.Add("Tesla");
+            Showroom.Add("Murcielago");
+            Showroom.Add("Wraith");
+            Showroom.Add("Phantom");
+
+            Console.WriteLine(Showroom.Count);
+
+            Showroom.Add("Tesla");
+
+            HashSet<string> UsedLot = new HashSet<string>();
+            UsedLot.Add("Ford Focus");
+            UsedLot.Add("Toyota Camry");
+
+            Showroom.UnionWith(UsedLot);
+
+            Showroom.Remove("Ford Focus");
+            foreach(string car in Showroom){
+                Console.WriteLine(car);
+            }
+
+            HashSet<string> Junkyard = new HashSet<string>() {
+                "Tesla", "Toyota Camry", "Hummer", "Beamer"
+            };
+
+            Showroom.IntersectWith(Junkyard);
+            Showroom.UnionWith(Junkyard);
+            Showroom.Remove("Tesla");
+            Showroom.Remove("Toyota Camry");
+
+            foreach(string car in Showroom){
+                Console.WriteLine(car + " x");
+            }
+        }
+    }
+}
